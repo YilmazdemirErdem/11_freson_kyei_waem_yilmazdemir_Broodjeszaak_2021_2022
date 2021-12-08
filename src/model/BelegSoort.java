58d21017bf0fa1632b105f0844ca58930101c34f
MattiasWaem <75215270+MattiasWaem@users.noreply.github.com>
2021-12-08 18:33:05 +0100

    Model init

---
+++ src/model/BelegSoort.java
@@ -0,0 +1,8 @@
+package model;
+
+import java.util.List;
+import java.util.TreeMap;
+
+public class BelegSoort {
+
+}
--- src/view/AdminMainPane.java
+++ src/view/AdminMainPane.java
@@ -28,3 +28,4 @@ public class AdminMainPane extends BorderPane {
         this.setCenter(tabPane);
     }
 }
+
