2c9cb00c7c8a0c2615c99629d8ac46cf71db7a5c
Erdem <erdem.yilmazdemir@studen.ucll.be>
2021-12-29 19:39:21 +0100

    Actual Final Countdown (AFC)

--- src/controller/StatistiekViewController.java
+++ src/controller/StatistiekViewController.java
@@ -1,8 +1,15 @@
 package controller;
 
+import utilities.Observer;
+
 /**
  * @Author: Mattias Waem, Erdem Yilmazdemir, Yannic Freson, Dazzy Kyei
  */
 
-public class StatistiekViewController {
+public class StatistiekViewController implements Observer {
+
+    @Override
+    public void update(int nrBestelling, int aantalBroodjes, double totalePrijs, int aantalBroodjesInWachtrij) {
+
+    }
 }
